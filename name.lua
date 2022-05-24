03095327e47a58e8455f310c3a3f10e6812dd183
death <91166315+Moo389@users.noreply.github.com>
2022-05-24 22:26:48 +0100

    Update name.lua

--- name.lua
+++ name.lua
@@ -51,10 +51,11 @@ local cool = { -- Userid, Title, Color (1 = rainbow, 0 = no rainbow, 2 = pink, 3
     {3065223306, "HoldingW", 3},
     {2435027122, "kroneW", 1},
     {2338962625, "sky / flopa", 1},
+    {142804804, "cute femboy cotra", 1},
     {1752174289, "skulldrain", 1},
     {128715333, "GlockW", 1},
     {2348564884, "legendary", 1},
-    {142804804, "Nigger", 4}
+    {1964094116, "Nigger", 4}
 }
 
 local function isCool(player)
