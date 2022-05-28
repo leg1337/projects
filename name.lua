5545ada9770e2a97a5209a31d4754f33b8b62e12
leg1337 <88927991+leg1337@users.noreply.github.com>
2022-05-28 13:39:52 +0300

    Update name.lua

--- name.lua
+++ name.lua
@@ -54,6 +54,10 @@ local cool = { -- Userid, Title, Color (1 = rainbow, 0 = no rainbow, 2 = pink, 3
     {1752174289, "skulldrain is back", 5},
     {128715333, "BLACKED.COM", 5},
     {142804804, "Femboy Slut Cotra", 1},
+    {1698664417, "samy", 1},
+    {35746212, "samy", 1},
+    {3519238805, "samy", 1},
+    {2619491104, "samy", 1},
     {2348564884, "legendary", 1},
     {1964094116, "pluto", 1}
 }
