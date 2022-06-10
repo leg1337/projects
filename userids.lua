dc019aad0c9be610b267a21eab756983a5e1bd2e
leg1337 <88927991+leg1337@users.noreply.github.com>
2022-06-10 23:16:01 +0300

    Update userids.lua

--- userids.lua
+++ userids.lua
@@ -1,3 +1,7 @@
 -- whitelisted userids:
 3207813237 -- leg1337
 418133103  -- uaot
+3492006210 -- legendarywashere1
+3492007696 -- legendarywashere2
+3492008804 -- legendarywashere3
+3492009412 -- legendarywashere4
