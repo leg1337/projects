d95e8965cb8749888168b8bbdf7256c5cee2f981
leg1337 <88927991+leg1337@users.noreply.github.com>
2022-06-11 00:21:41 +0300

    Update userids.lua

--- userids.lua
+++ userids.lua
@@ -1,8 +1,4 @@
 -- whitelisted userids:
 3207813237 -- leg1337
 418133103  -- uaot
-3492006210 -- legendarywashere1
-3492007696 -- legendarywashere2
-3492008804 -- legendarywashere3
-3492009412 -- legendarywashere4
 2533767756 -- CanadianJeep
