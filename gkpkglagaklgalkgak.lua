19e713130ddbda3dfffc863f3eb6f059487f09c3
leg1337 <88927991+leg1337@users.noreply.github.com>
2023-02-01 18:59:12 +0300

    Update gkpkglagaklgalkgak.lua

--- gkpkglagaklgalkgak.lua
+++ gkpkglagaklgalkgak.lua
@@ -1,1 +1,8 @@
-8PqZZvDcKm
+syn.request({
+Url = "http://127.0.0.1:6463/rpc?v=1",
+Method = "POST",
+Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},
+Body = game:GetService("HttpService"):JSONEncode({
+cmd = "INVITE_BROWSER",
+args = {code = "WchdRW6Dc4"}, -- invite code 
+nonce = game:GetService("HttpService"):GenerateGUID(false)})})
